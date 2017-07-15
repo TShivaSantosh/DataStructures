36337d62b5b6766388a6c1c94f0ebe26be7cf44b
shivasantosh tangirala <shiva_santosh01@yahoo.co.in>
2017-07-15 19:19:23 +0530

    +creating node for linked list

--- DataStructures.playground/Contents.swift
+++ DataStructures.playground/Contents.swift
@@ -2,3 +2,27 @@
 
 import UIKit
 print("Hello, Data Structures")
+
+
+/*
+    Single Linked List
+*/
+
+
+class Node{
+    let data:Int!
+    var next:Node?
+    init(_ data:Int)
+    {
+        self.data = data
+    }
+}
+
+
+
+
+
+
+
+
+
